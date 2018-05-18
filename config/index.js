7e046b4d4e5e055780ef9cbc2bde4fe9adb7de33
2014874201 <1152579039@qq.com>
2018-05-18 14:12:39 +0800

    1

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: '192.168.10.183', // can be overwritten by process.env.HOST
+    port: 8881, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
@@ -22,7 +22,7 @@ module.exports = {
 
     
     /**
-     * Source Maps
+     * Source Maps  
      */
 
     // https://webpack.js.org/configuration/devtool/#development
